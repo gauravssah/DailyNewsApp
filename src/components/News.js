a1119e578e9559adad0e2d21a6b9afea3cb17cfe
gauravshonik <gauravshonik01@gmail.com>
2023-05-28 17:36:03 +0530

    starter files added

---
+++ src/components/News.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import NewsItems from './NewsItems'
+
+export class News extends Component {
+    render() {
+        return (
+            <div>
+
+                news box
+
+                <NewsItems />
+
+            </div>
+        )
+    }
+}
+
+export default News
---
+++ src/components/NewsItems.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class NewsItems extends Component {
+    render() {
+        return (
+            <div>
+                news is here....
+
+            </div>
+        )
+    }
+}
+
+export default NewsItems
